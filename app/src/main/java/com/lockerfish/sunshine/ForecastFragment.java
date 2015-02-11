c71dc45e13b14122cbfedc5285b12b73e93882af
Hendrix Tavarez <htavarez@gmail.com>
2015-02-11 15:12:30 -0500

    Added DetailActivity

--- app/src/main/java/com/lockerfish/sunshine/ForecastFragment.java
+++ app/src/main/java/com/lockerfish/sunshine/ForecastFragment.java
@@ -10,10 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.AsyncTask;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.content.Intent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -76,6 +80,22 @@ public class ForecastFragment extends Fragment {
 
         ListView list = (ListView)rootView.findViewById(R.id.listview_forecast);
         list.setAdapter(mForecastAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        	@Override
+        	public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        		String item = (String)adapterView.getItemAtPosition(i);
+
+        		// Toast toast = Toast.makeText(getActivity(), item, Toast.LENGTH_SHORT);
+        		// toast.show();
+
+        		Intent intent = new Intent(getActivity(), DetailActivity.class);
+        		intent.putExtra(Intent.EXTRA_TEXT, item);
+        		startActivity(intent);
+
+        	}
+        });
 
         return rootView;
     }
